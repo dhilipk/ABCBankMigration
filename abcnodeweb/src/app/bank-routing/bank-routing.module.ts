f5e316f81a80393b3909b206fb5e5bf9b45ddbe2
Jones Bastin Xavier Edvinraja <jonese@virtusapolaris.com>
2017-08-20 01:17:18 +0530

    Updated router details.

--- abcnodeweb/src/app/bank-routing/bank-routing.module.ts
+++ abcnodeweb/src/app/bank-routing/bank-routing.module.ts
@@ -4,13 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { SignInComponent } from '../sign-in/sign-in.component';
 import { AccountsComponent } from '../accounts/accounts.component';
+import { AccountStatementComponent } from '../accounts/account-statement/account-statement.component';
 import { AuthenticateGuard } from '../guard/authenticate.guard';
 
 const routes: Routes = [
     { path: 'aboutUs', component: DashboardComponent },
     { path: 'signIn', component: SignInComponent },
-    { path: 'accounts', component: AccountsComponent, canActivate: [AuthenticateGuard] },
     { path: 'accounts/summary', component: AccountsComponent, canActivate: [AuthenticateGuard] },
+    { path: 'accounts/viewStatement', component: AccountStatementComponent, canActivate: [AuthenticateGuard] },
     { path: '**', redirectTo: 'aboutUs' }
 ];
 
