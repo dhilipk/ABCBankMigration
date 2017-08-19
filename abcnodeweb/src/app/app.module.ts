13412ff3351a668bbe6806f3dfa89af21f51c067
Dhilip Kumar <dhilip.dina@gmail.com>
2017-08-19 18:02:44 +0530

    Routing Added

--- abcnodeweb/src/app/app.module.ts
+++ abcnodeweb/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BankRoutingModule } from './bank-routing/bank-routing.module';
+
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BankRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
