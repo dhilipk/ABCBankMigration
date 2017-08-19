6430d576a5ceb53e132d84a666b374d692d7f1f3
manojc <christo1424@gmail.com>
2017-08-20 02:31:51 +0530

    Account Summary Service Integration

---
+++ abcnodeweb/src/app/sign-in/authentication.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthenticationService {
+  public token: string;
+
+  constructor(private http: Http) {
+    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.token = currentUser && currentUser.token;
+  }
+
+  login(username: string, password:string) : Observable<Boolean> {
+    let headers = new Headers({ 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post('http://localhost:8083/v1/user/authenticate', JSON.stringify({ username: username, password: password }), options)
+      .map((response: Response) => {
+        let token = response.json() && response.json().token;
+        if(token) {
+          this.token = token;
+          localStorage.setItem('currentUser', JSON.stringify({ username: username, token: this.token }));
+          return true;
+        } else {
+          return false;
+        }
+    });
+  }
+
+  logout(): void {
+      // clear token remove user from local storage to log user out
+      this.token = null;
+      localStorage.removeItem('currentUser');
+  }
+}
