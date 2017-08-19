e8931f5c39edad9e7c790c47b61f4aca759fab96
berkarul <berkarul@gmail.com>
2017-08-19 22:28:51 +0530

    Account Summary Service Added

---
+++ AccountsService/src/main/java/com/bfs/service/AccountsService.java
@@ -0,0 +1,45 @@
+package com.bfs.service;
+
+import javax.ws.rs.QueryParam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bfs.service.entity.Accounts;
+
+
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountsService {
+
+		
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsService.class, args);
+	}
+	
+	@RequestMapping(value = "/v1/accounts/summary", method = RequestMethod.GET)
+    public Accounts register(@QueryParam("username") String username) {
+		Accounts account;
+		if (username.equalsIgnoreCase("dhilip")) {
+			account = new Accounts("Dhilip Kumar", "565896321455", "Kodampakkam", "8600", "Current");
+		} else if (username.equalsIgnoreCase("arul")) {
+			account = new Accounts("Berchmans Arul", "345256784545", "Nungampakkam", "8600", "Savings");
+		} else if (username.equalsIgnoreCase("manoj")) {
+			account = new Accounts("Manoj Christopher", "849256784545", "OMR", "8600", "Savings");
+		} else if (username.equalsIgnoreCase("logesh")) {
+			account = new Accounts("Logeswaran", "655146784596", "T-Nagar", "8600", "Savings");
+		}
+		else {
+			account = new Accounts("Jones Bastin Xavier", "985256784335", "Chennai", "8600", "Current");
+		}
+		
+		return account;
+    }
+	
+	
+}
