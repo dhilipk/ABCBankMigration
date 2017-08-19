4b2aa1daf879ca09736e2c2040384f2b335c3d25
Jones Bastin Xavier Edvinraja <jonese@virtusapolaris.com>
2017-08-20 01:47:23 +0530

    Added Cob and Banking templates.

---
+++ abcnodeweb/src/app/banking/benificiary/benificiary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-benificiary',
+  templateUrl: './benificiary.component.html',
+  styleUrls: ['./benificiary.component.scss']
+})
+export class BenificiaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
