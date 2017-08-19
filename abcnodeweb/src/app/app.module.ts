cdc03f34f7ea63d9ce1e7a80e3b6f9085d171d65
Jones Bastin Xavier Edvinraja <jonese@virtusapolaris.com>
2017-08-19 18:36:03 +0530

    Updated sign in page.

--- abcnodeweb/src/app/app.module.ts
+++ abcnodeweb/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { BankRoutingModule } from './bank-routing/bank-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
